5ab215a12d22d526252f9bcf0a3b5272aec8fcc5
vladimir <babenkovladimirbmd@gmail.com>
2019-02-17 11:07:11 +0200

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,43 @@
+import 'package:prompter_sg/prompter_sg.dart';
+import 'dart:io';
+import 'package:converter/src/converter.dart';
+
+void main() {
+  final prompter = new Prompter();
+
+  final choice = prompter.askBinary('Are you here to convert  an image?');
+
+  if (!choice) {
+    exit(0);
+  }
+
+  final format = prompter.askMultiple('Select format', buildFormatOptions());
+
+  final selectedFile =  prompter.askMultiple('Select an image to convert', buildFileOptions());
+
+  final newPath =  convertImage(selectedFile, format);
+
+  final shouldOpen = prompter.askBinary('Open the image ?');
+
+  if(shouldOpen){
+    Process.run('open', [newPath]);
+  }
+}
+
+List<Option> buildFormatOptions() {
+  return [
+    new Option('Convert to jpeg', 'jpeg'),
+    new Option('Convert to png', 'png'),
+  ];
+}
+
+buildFileOptions() {
+  Directory.current.listSync().where((entity) {
+    return FileSystemEntity.isFileSync(entity.path) &&
+        entity.path.contains(new RegExp(r'\.(png|jpj|jpeg)'));
+  }).map((entity) {
+    final fileName = entity.path.split(Platform.pathSeparator).last;
+
+    return new Option(fileName, entity);
+  }).toList();
+}
